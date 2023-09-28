ef41a77fb2a82b06cd13d44b92b0a4740f2d339e
Messai <messai@github.com>
2023-09-28 11:18:33 +0300

    Done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,6 +10,9 @@ def pascal_triangle(n):
     Returns a list of lists of integers representing
     the Pascal's triangle of n
     """
+    if n <= 0:
+        return []
+
     outer_list = []
     for n in range(n):
         inner_list = []
