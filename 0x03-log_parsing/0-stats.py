d1ea94d26d14982d7867e739260bd59f552b71fc
Messai <messai@github.com>
2023-10-19 12:39:34 +0300

    stats

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Reads stdin line by line and computes metrics
+"""
+import sys
+
+lines = sys.stdin
+
+def read_n_lines(n):
+    return [sys.stdin.readline().rstrip() for _ in range(n)]
+
+while lines:
+    totalSize = 0
+    count = 0
+    codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    lines = read_n_lines(10)
+    while count < 10:
+        line = lines[count]
+        arg = line.split(" ")
+        totalSize += int(arg[-1])
+        for code in codes.keys():
+            if code == arg[-2]:
+                codes[code] += 1
+        count += 1
+
+    print('File size: {}'.format(totalSize))
+    for code, value in sorted(codes.items()):
+        if value != 0:
+            print('{}: {}'.format(code, value))
