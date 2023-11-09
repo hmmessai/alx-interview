80a486a6c438f51e7850480b062c230f25cad264
Messai <hmmesai@github.com>
2023-11-09 05:08:02 -0800

    correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,30 @@
 const request = require('request');
 
 if (process.argv.length < 4) {
-const movie_id = process.argv[2]
-let url = 'https://swapi-api.alx-tools.com/api/films/';
+  const movieId = process.argv[2];
+  let url = 'https://swapi-api.alx-tools.com/api/films/';
 
-if (movie_id) {
-  url = 'https://swapi-api.alx-tools.com/api/films/' + movie_id;
-}
+  if (movieId) {
+    url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+  }
 
-request(url, (error, response, body) => {
+  request(url, (error, response, body) => {
     if (error) {
-      console.log(err);
+      console.log(error);
     }
     const characters = JSON.parse(body).characters;
-    for (character of characters) {
-      request(character, (err, resp, bod) => {
-	 if (err) {
-	   console.log(err)
-	 }
-         let name = JSON.parse(bod).name;
-	 console.log(name);
-      })
-    }
-});
+    const charactersName = characters.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (err, resp, bod) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(JSON.parse(bod).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
 }
