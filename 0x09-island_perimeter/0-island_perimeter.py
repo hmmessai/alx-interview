641aa36df5bb528165ec2a3d51563e67488e29bd
Messai <messai@github.com>
2023-11-29 19:30:17 +0300

    done with all the tasks

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define island perimeter
+"""
+
+def island_perimeter(grid):
+    """Computes perimeter of the grid
+    """
+    totPerim = 0
+    connections = 0
+    for row in grid:
+        for col in row:
+            if col == 1:
+                totPerim += 4
+
+    for row in range(len(grid)):
+        for i in range(len(grid[row])):
+            if grid[row][i] == 1:
+                if grid[row][i-1] == 1:
+                    connections += 2
+                if grid[row - 1][i] == 1: 
+                    connections += 2
+
+    return (totPerim - connections)
